b40a9e765647c8082eaa99fcc2b394adf8ce2437
josafamarengo <josafabmarengo@gmail.com>
2022-11-06 08:18:15 -0400

    feat: add navigation

---
+++ src/navigation/index.js
@@ -0,0 +1,113 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import HomeScreen from '../screens/HomeScreen';
+import RestaurantDetailsScreen from '../screens/RestaurantDetailsScreen';
+import DishDetailsScreen from '../screens/DishDetailsScreen';
+import BasketScreen from '../screens/BasketScreen';
+import OrdersScreen from '../screens/OrdersScreen';
+import OrderDetailsScreen from '../screens/OrderDetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+return (
+    <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: 'false'}}>
+        <Stack.Screen 
+            name="Home" 
+            component={TabNavigation}
+        />
+        <Stack.Screen 
+            name="Dish" 
+            component={DishDetailsScreen} 
+        />
+        <Stack.Screen 
+            name="Basket" 
+            component={BasketScreen} 
+        />
+        <Stack.Screen 
+            name="Orders" 
+            component={OrdersScreen} 
+        />
+        <Stack.Screen 
+            name="OrderDetails" 
+            component={OrderDetailsScreen} 
+        />
+    </Stack.Navigator>
+    );
+}
+
+const Tab = createMaterialBottomTabNavigator();
+
+const TabNavigation = () => {
+    return (
+        <Tab.Navigator barStyle={{ backgroundColor: 'white'}}>
+            <Tab.Screen
+                name="Home"
+                component={HomeStackScreen}
+                options={{
+                    tabBarLabel: 'Início',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Orders"
+                component={OrdersScreen}
+                options={{
+                    tabBarLabel: 'Histórico',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="clipboard-list" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Basket"
+                component={BasketScreen}
+                options={{
+                    tabBarLabel: 'Em andamento',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="basket" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: 'Meu perfil',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="account" color={color} size={26} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+
+const HomeStack = createNativeStackNavigator();
+
+const HomeStackScreen = () => {
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen 
+                name="Início" 
+                component={HomeScreen}
+                options={{
+                    headerShown: false
+                }}
+            />
+            <HomeStack.Screen 
+                name="Restaurant"
+                component={RestaurantDetailsScreen}
+                options={{
+                    headerShown: false
+                }}
+            />
+        </HomeStack.Navigator>
+    );
+}
+
+export default Navigation;
